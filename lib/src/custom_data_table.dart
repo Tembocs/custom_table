4665f5a711c782f0f147844af1002d4945a6769b
Tembo Nyati <tembocs101@gmail.com>
2024-04-05 20:51:43 +0300

    Refector CustomTable for better optimization.

--- lib/src/custom_data_table.dart
+++ lib/src/custom_data_table.dart
@@ -2,6 +2,120 @@ import 'package:flutter/material.dart';
 import 'table_row_data.dart';
 
 class CustomTable extends StatefulWidget {
+  final EdgeInsetsGeometry padding;
+  final List<String> columns;
+  final List<List<TableRowData>> data;
+  final double rowHeight;
+  final List<double> columnWidths;
+
+  const CustomTable({
+    super.key,
+    required this.columns,
+    required this.data,
+    this.padding = EdgeInsets.zero, // Default padding set to zero
+    this.rowHeight = 40.0,
+    this.columnWidths = const [], // Default to an empty list
+  });
+
+  CustomTable.fromMapList({
+    super.key,
+    required List<String> columns,
+    required List<Map<String, String>> mapList,
+    EdgeInsetsGeometry padding = EdgeInsets.zero,
+    double rowHeight = 40.0,
+    List<double> columnWidths = const [],
+  })  : this.padding = padding,
+        this.rowHeight = rowHeight,
+        this.columnWidths = columnWidths,
+        this.columns = columns,
+        this.data = mapList.map((aMap) {
+          return aMap.entries.map((entry) {
+            return TableRowData(entry.key, Text(entry.value));
+          }).toList();
+        }).toList();
+
+  @override
+  State createState() => _CustomTableState();
+}
+
+class _CustomTableState extends State<CustomTable> {
+  Widget _buildTableHeader() {
+    return SizedBox(
+      height: widget.rowHeight,
+      child: Row(
+        children: widget.columns.map((columnName) {
+          final index = widget.columns.indexOf(columnName);
+          return Expanded(
+            flex: _getColumnFlex(index),
+            child: Text(columnName),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _buildTableRow(List<TableRowData> rowData, int index) {
+    return Container(
+      height: widget.rowHeight,
+      color: index % 2 == 0 ? Colors.grey[200] : Colors.white,
+      child: Row(
+        children: rowData.map((cellData) {
+          final colIndex = rowData.indexOf(cellData);
+          return Expanded(
+            flex: _getColumnFlex(colIndex),
+            child: cellData.value,
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  int _getColumnFlex(int index) {
+    if (index < widget.columnWidths.length) {
+      return (widget.columnWidths[index] * 100).floor();
+    }
+
+    // Default flex value
+    return 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: widget.padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Divider(
+            color: Colors.grey,
+            thickness: 1.0,
+          ),
+          _buildTableHeader(),
+          const Divider(
+            color: Colors.grey,
+            thickness: 0.5,
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: widget.data.length,
+              itemBuilder: (context, index) {
+                return _buildTableRow(widget.data[index], index);
+              },
+              separatorBuilder: (context, index) => const Divider(
+                color: Colors.grey,
+                height: 0.5,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*
+class CustomTable extends StatefulWidget {
   final EdgeInsetsGeometry? padding;
   final List<String> columns;
   final List<List<TableRowData>> data;
@@ -29,15 +143,6 @@ class CustomTable extends StatefulWidget {
         this.rowHeight = rowHeight,
         this.columnWidths = columnWidths,
         this.columns = columns,
-        // this.data = mapList.map<List<TableRowData>>((map) {
-        //   return columns.map<TableRowData>((columnName) {
-        //     // Now correctly initializing TableRowData with columnName and value
-        //     return TableRowData(
-        //       columnName,
-        //       Text(map[columnName] ?? 'N/A'),
-        //     );
-        //   }).toList();
-        // }).toList();
         this.data = mapList.map((aMap) {
           return aMap.entries.map((entry) {
             // Creating a TableRowData for each entry
@@ -129,3 +234,4 @@ class _CustomTableState extends State<CustomTable> {
     );
   }
 }
+*/
